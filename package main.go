b74047ed9f36eaaafb21202ccd1b6f53796e08ca
AleksandrKochanov <alexandr_kavlirbis@mail.ru>
2023-07-23 00:17:09 +0300

    commit_2

--- package main.go
+++ package main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var str string = "Переменная"
 	fmt.Println("Как-то нужно начинать")
-	fmt.Println("Например, так")
+	fmt.Println("Например, так...")
 	fmt.Println(str)
 	some_int := 12
 	fmt.Println(some_int)
