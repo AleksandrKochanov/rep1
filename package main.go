e2344d28aac06fd33322e347c834b57d61d248be
AleksandrKochanov <alexandr_kavlirbis@mail.ru>
2023-07-25 00:30:02 +0300

    That's another one

--- package main.go
+++ package main.go
@@ -5,13 +5,18 @@ import (
 )
 
 func main() {
-	var str string = "Переменная"
-	fmt.Println("Как-то нужно начинать")
-	fmt.Println("Например, так...")
-	fmt.Println(str)
-	some_int := 12
-	fmt.Println(some_int)
-	fmt.Println("Очередной текст")
+	//	var str string = "Переменная"
+	//	fmt.Println("Как-то нужно начинать")
+	//	fmt.Println("Например, так...")
+	//	fmt.Println(str)
+	//	some_int := 12
+	//	fmt.Println(some_int)
+	//	fmt.Println("Очередной текст")
+	var number int
+	fmt.Scan(&number)
+	fmt.Println(number)
+	number++
+	fmt.Println(number)
 }
 
 //Какой-то однострочный комментарий
