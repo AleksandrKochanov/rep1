600e8eaa7ff96725852f1e52866dc89e3fbeb1fd
AleksandrKochanov <alexandr_kavlirbis@mail.ru>
2023-07-27 23:41:23 +0300

    Let's look what we got

---
+++ first_try/package main.first_try.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	var str string = "Переменная"
+	//	fmt.Println("Как-то нужно начинать")
+	//	fmt.Println("Например, так...")
+	//	fmt.Println(str)
+	//	some_int := 12
+	//	fmt.Println(some_int)
+	//	fmt.Println("Очередной текст")
+	var n1, n2, n3 int
+	fmt.Scan(&n1, &n2, &n3)
+	fmt.Println(n1, n2, n3)
+	//	number++
+	//	fmt.Println(number)
+	//	var n int32 = 'c'
+	//	fmt.Println(n)
+
+}
+
+//Какой-то однострочный комментарий
+/*
+Какой-то
+многострочный
+комментарий
+*/
---
+++ some_experiment/package main.some_experiment.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 10
+	if 8 <= x && x <= 12 {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+}
